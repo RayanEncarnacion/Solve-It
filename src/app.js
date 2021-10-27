04d4547b60a99b44e1fe20d8ae6572dbbc3991a1
Rayan Encarnacion <rayanbalbuena@gmail.com>
2021-10-27 18:58:50 -0400

    Separate module for logout funcionality

--- src/app.js
+++ src/app.js
@@ -1,15 +1,4 @@
 import { initMainMenu } from "./js/helpers";
+import logout from "./js/requestHandlers/logout-confirmation";
 initMainMenu();
-
-const logOutBtn = document.querySelector(".log-out");
-
-const logoutConfirmation = () => {
-  let confirmAction = confirm("Are you sure you want to Log out?");
-
-  if (confirmAction) {
-    localStorage.removeItem("user");
-    window.location.href = "html/login.html";
-  } else return;
-};
-
-logOutBtn.addEventListener("click", logoutConfirmation);
+logout();
---
+++ src/js/requestHandlers/logout-confirmation.js
@@ -0,0 +1,14 @@
+const logOutBtn = document.querySelector(".log-out");
+
+const logoutConfirmation = () => {
+  let confirmAction = confirm("Are you sure you want to Log out?");
+
+  if (confirmAction) {
+    localStorage.removeItem("user");
+    window.location.href = "html/login.html";
+  } else return;
+};
+
+export default function () {
+  logOutBtn.addEventListener("click", logoutConfirmation);
+}
